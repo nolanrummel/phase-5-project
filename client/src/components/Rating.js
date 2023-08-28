ade4cf21b551909367596d9936aa485ac826ea64
Nolan Rummel <nolan.rummel22@gmail.com>
2023-08-28 01:40:38 -0400

    rides page mostly done, routes started

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -2,10 +2,11 @@ import React, { useContext, useState } from 'react'
 import { UserContext } from '../context/user'
 import "../styling/rating.css"
 
-function Rating({rating, pastPublic, rideId}) {
+function Rating({rating, rideId, detailRide, setRideChanges}) {
     const { user } = useContext(UserContext)
 
     const [starRating, setStarRating] = useState(0)
+    const [newRating, setNewRating] = useState(0)
     const [editing, setEditing] = useState(false)
 
     const handleStarClick = (selectedRating) => {
@@ -22,55 +23,84 @@ function Rating({rating, pastPublic, rideId}) {
         arrayLength = 5 - averageInteger
     }
 
-    const handleRatingChange = (e) => {
+    const handleEditState = (e) => {
+        e.stopPropagation()
         setEditing(!editing)
     }
 
     const confirmRating = (e) => {
-        e.preventDefault()
+        e.stopPropagation()
+        // e.preventDefault()
+
         console.log(rideId)
         const formObj = {
-            'rating': rating
+            'rating': starRating
         }
-        fetch(`http://127.0.0.1:5555/rides/${rideId}`, {
-            method: 'PATCH',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formObj)
-        })
-            .then(r => {
-                if(r.ok) {
-                    r.json()
-                        .then(data => {
-                            console.log(data)
-                        })
-                }
-                else {
-                    r.json()
-                        .then(data => {
-                            console.log(data)
-                        })
-                }
+        fetch(`http://127.0.0.1:5555/rides/${rideId.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formObj),
+        }).then(r => {
+        if (r.ok) {
+            r.json().then(data => {
+                console.log(data)
             })
+        } else {
+            r.json().then(data => {
+                console.log(data)
+            })
+        }})
+        // setRideChanges(starRating)
     }
 
     return (
         <div>
-            {editing ? 
+            {detailRide ? 
                 <div>
-                    {Array.from({length: 5}, (_, index) => (
-                        <span
-                            key={index}
-                            onClick={() => handleStarClick(index +1)}
-                            className={index < starRating ? 'edit-filled-star' : 'edit-empty-star'}
-                        >
-                            ★
-                        </span>
-                    ))}
-                    <button onClick={confirmRating}>Confirm Changes</button>
-                    <button onClick={handleRatingChange}>Cancel Editing</button>
+                    {editing ? 
+                        <div>
+                            {Array.from({length: 5}, (_, index) => (
+                                <span
+                                    key={index}
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        handleStarClick(index + 1);
+                                    }}
+                                    className={index < starRating ? 'edit-filled-star' : 'edit-empty-star'}
+                                >
+                                    ★
+                                </span>
+                            ))}
+                            <div>
+                                <button onClick={confirmRating}>Confirm Changes</button>
+                                <button onClick={handleEditState}>Cancel Editing</button>
+                            </div>
+                        </div>
+                        :
+                        <div className='rating-lockup'>
+                            <div>
+                                {Array.from({length: averageInteger}, (_, index) => (
+                                    <span key={index} className='filled-star'>
+                                        ★
+                                    </span>
+                                ))}
+                                {averageDecimal == 0 ? '':
+                                    <span className='fractional-star' style={{background: `linear-gradient(to right, gold ${averageDecimal}%, red ${averageDecimal}%)`, '-webkit-background-clip': 'text'}}>
+                                        ★
+                                    </span>
+                                }
+                                {Array.from({length: arrayLength}, (_, index) => (
+                                    <span key={index} className='empty-star'>
+                                        ★
+                                    </span>
+                                ))}
+                            </div>
+                            <button className='change-rating' onClick={handleEditState}>Change Rating</button>
+                        </div>
+                    }
                 </div>
                 :
-                <div>
+                <div className='rating-lockup'>
                     <div>
                         {Array.from({length: averageInteger}, (_, index) => (
                             <span key={index} className='filled-star'>
@@ -87,10 +117,16 @@ function Rating({rating, pastPublic, rideId}) {
                                 ★
                             </span>
                         ))}
-                        {pastPublic ? '' : <button onClick={handleRatingChange}>Change Rating</button>}
                     </div>
                 </div>
             }
+            {/* {user !== null ? 
+                            <div>
+                                {detailRide === rideId ? '' : <button className='change-rating' onClick={handleRatingChange}>Change Rating</button> }
+                            </div>
+                            :
+                            ''
+                        } */}
         </div>  
     )
 }
--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context/user'
-import Rating from './Rating'
+import LeaderBoard from './LeaderBoard'
 import "../styling/routes.css"
 
 function Routes({currentTime}) {
@@ -18,59 +18,6 @@ function Routes({currentTime}) {
           })
     }, [])
 
-    const LeaderBoard = (route) => {
-        const rides = route.rides
-        const userCount = {}
-        const completedRides = rides.filter(item => item.date < currentTime)
-        let totalRating = 0
-        completedRides.forEach(rideData => {
-            const userId = rideData.user.id
-            totalRating += rideData.rating
-            if (userId in userCount) {
-                userCount[userId]++
-            } else {
-                userCount[userId] = 1
-            }
-        })
-        const averageRating = (totalRating/completedRides.length).toFixed(2)
-        const sortedLeaders = Object.keys(userCount).map(userId => ({
-            id: userId,
-            name: rides.find(rideData => rideData.user.id === parseInt(userId)).user.name,
-            count: userCount[userId]
-        }))
-        sortedLeaders.sort((a, b) => b.count - a.count)
-        const slicedLeaders = sortedLeaders.slice(0, 5)
-        return (
-            <div>
-                {slicedLeaders.map(leader => {
-                    return (
-                        <div key={leader.id}>
-                            {user ? 
-                                <div>
-                                    {user.id === parseInt(leader.id) ?
-                                        <div key={leader.id} className='user-leader'>
-                                            <p>{leader.name}: {leader.count} | Total Miles: {(leader.count * route.distance).toFixed(2)}</p>
-                                        </div>
-                                        :
-                                        <div key={leader.id}>
-                                            <p>{leader.name}: {leader.count} | Total Miles: {(leader.count * route.distance).toFixed(2)}</p>
-                                        </div>
-                                    }
-                                </div>
-                                :
-                                <div key={leader.id}>
-                                    <p>{leader.name}: {leader.count} | Total Miles: {(leader.count * route.distance).toFixed(2)}</p>
-                                </div>
-                            }
-                        </div>
-                    )
-                })}
-                <p>Total Trips: {completedRides.length}</p>
-                <p>Average Rating: {averageRating}<Rating rating={averageRating}/>({completedRides.length})</p>
-            </div>
-        )
-    }
-
     const renderRoutes = routeList.map((route) => {
         return (
             <div key={route.id}>
@@ -78,7 +25,8 @@ function Routes({currentTime}) {
                 <h5>{route.distance} Miles</h5>
                 <div>
                     <h5>Leader Board</h5>
-                    {LeaderBoard(route)}
+                    {/* {LeaderBoard(route)} */}
+                    <LeaderBoard route={route} currentTime={currentTime}/>
                 </div>
             </div>
         )
@@ -86,7 +34,6 @@ function Routes({currentTime}) {
 
     return (
         <div>
-            <h2>Routes Page</h2>
             {renderRoutes}
         </div>  
     )
